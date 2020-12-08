e5ab156ace85ab682ee63b9525ee658e6a8ce6ee
Jinny Jang <jangjinny21@gmail.com>
2020-12-08 12:09:27 +0000

    create a function to determine odd number

---
+++ lighthouse-js-fundamentals/odd.js
@@ -0,0 +1,15 @@
+/*
+const isEven  = function (num) {
+  return num % 2 === 0;
+} 
+
+console.log(isEven(10));
+console.log(isEven(11));
+*/
+
+const isOdd = function (number) {
+  return number % 2 !== 0;
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
